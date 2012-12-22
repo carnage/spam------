f03df98b31d3a1878accde8badeba60438047e61
Carnage <t.carnage@gmail.com>
2012-12-22 19:40:47 +0000

    Added ip field to userdata;

--- library/SpamLib/User/Abstract.php
+++ library/SpamLib/User/Abstract.php
@@ -4,6 +4,7 @@ class SpamLib_User_Abstract extends SpamLib_DataObject_Abstract
 	protected $_validfields = array(
 		'postcount',
 		'username',
-		'registrationdate'		
+		'registrationdate',
+		'ip'		
 	);
 }
